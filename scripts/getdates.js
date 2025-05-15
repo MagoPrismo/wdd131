e9710fad1df32b0926cdcc056f64930be73d4a57
MagoPrismo <sgngms3@gmail.com>
2025-05-15 15:01:25 -0300

    year

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,5 @@
+const today = new Date();
+const yearSpan = document.querySelector("#year");
+yearSpan.textContent = new Date().getFullYear();
+
 document.getElementById("lastModified").textContent = document.lastModified;

0b0f45452b10a7759b02226d793741ba0b24dcc8
MagoPrismo <sgngms3@gmail.com>
2025-05-20 16:01:24 -0300

    week 2

---
+++ scripts/temples.js
@@ -0,0 +1,17 @@
+//code for the footer
+const today = new Date();
+const yearSpan = document.querySelector("#year");
+yearSpan.textContent = new Date().getFullYear();
+
+document.getElementById("lastModified").textContent = document.lastModified;
+
+//code for the hamburguer menu
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu')
+
+hambutton.addEventListener('click', () =>
+{
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});

b8e52170a59d22e50b540a5fded6d485dc1e36c3
MagoPrismo <sgngms3@gmail.com>
2025-05-31 22:06:26 -0300

    js up

--- scripts/place.js
+++ scripts/place.js
@@ -8,17 +8,23 @@ document.getElementById("lastModified").textContent = document.lastModified;
 
 //code for the windchill//
 
-const temp = 0;
-const windspeed = 5;
+const temp = 9; //°C//
+const windspeed = 5; //km/h//
+
 const condition = "Partly Cloudy" 
 
 //windchill calculator//
 
-const windchill =  13.12 + 0.6215 * temp 
-                - 11.37 * Math.pow(windspeed,0.16)
-                + 0.3965 * temp * Math.pow(windspeed,0.16);
+if (temp <= 10 && windspeed >= 4.8) {
+    const windchill =  13.12 + 0.6215 * temp 
+                    - 11.37 * Math.pow(windspeed,0.16)
+                    + 0.3965 * temp * Math.pow(windspeed,0.16);
+                    document.getElementById("windchill").textContent = windchill.toFixed(2) + " °C";
+} else {
+    const windchill = "N/A";
+    document.getElementById("windchill").textContent = windchill;
+}
 
 document.getElementById("temp").textContent = temp;
 document.getElementById("condition").textContent = condition;
 document.getElementById("windspeed").textContent = windspeed;
-document.getElementById("windchill").textContent = windchill.toFixed(2);

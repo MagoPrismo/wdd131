ff011df09a35a377cd0657e4324dcbcc65c4634c
MagoPrismo <sgngms3@gmail.com>
2025-05-31 21:56:31 -0300

    js updates

--- scripts/place.js
+++ scripts/place.js
@@ -4,3 +4,21 @@ const yearSpan = document.querySelector("#year");
 yearSpan.textContent = new Date().getFullYear();
 
 document.getElementById("lastModified").textContent = document.lastModified;
+
+
+//code for the windchill//
+
+const temp = 0;
+const windspeed = 5;
+const condition = "Partly Cloudy" 
+
+//windchill calculator//
+
+const windchill =  13.12 + 0.6215 * temp 
+                - 11.37 * Math.pow(windspeed,0.16)
+                + 0.3965 * temp * Math.pow(windspeed,0.16);
+
+document.getElementById("temp").textContent = temp;
+document.getElementById("condition").textContent = condition;
+document.getElementById("windspeed").textContent = windspeed;
+document.getElementById("windchill").textContent = windchill.toFixed(2);

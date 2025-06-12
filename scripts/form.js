8a52e56e077350fbb914d9d9327253655d4e2f55
MagoPrismo <sgngms3@gmail.com>
2025-06-12 16:03:38 -0300

    week5

--- scripts/form.js
+++ scripts/form.js
@@ -1,6 +1,51 @@
-//code for the footer
+//code for the footer//
 const today = new Date();
 const yearSpan = document.querySelector("#year");
 yearSpan.textContent = new Date().getFullYear();
 
 document.getElementById("lastModified").textContent = document.lastModified;
+
+//product array//
+
+const products = [
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
+
+function createProduct(products) {
+    products.forEach(product => {
+        let card = document.createElement('option');
+        let name = product.name;
+
+        card.setAttribute('name', name);
+        card.setAttribute('value', name);
+        card.textContent = name;
+
+        document.querySelector('#product-name').appendChild(card);
+    }) 
+}
+
+createProduct(products);

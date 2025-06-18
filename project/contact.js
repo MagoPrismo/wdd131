3d719ecfc130b8d9471a901a45ca5039a6b696c5
MagoPrismo <sgngms3@gmail.com>
2025-06-18 17:45:03 -0300

    final ups

--- project/contact.js
+++ project/contact.js
@@ -4,3 +4,19 @@ const yearSpan = document.querySelector("#year");
 yearSpan.textContent = new Date().getFullYear();
 
 document.getElementById("lastModified").textContent = document.lastModified;
+
+const mainnav = document.querySelector('.navigation3');
+const hambutton = document.querySelector('#hidden-menu');
+
+hambutton.addEventListener('click', () =>
+{
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
+
+const screenSize = window.innerWidth;
+
+if (screenSize <= 500) {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+}
